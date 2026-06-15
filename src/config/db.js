332233b6c7c887ff87ab125e8dd7e91171639347
pedrohmartinsss <pepehenrique007.ph@gmail.com>
2026-06-15 12:04:15 -0300

    Use environment variables for database configuration

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ import mysql from "mysql2/promise";
 console.log("🔌 Iniciando conexão com MySQL...");
 
 export const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Ph07052002",
-    database: "orcamento",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10
 });
